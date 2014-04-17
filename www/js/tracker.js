b15590f15ca472a659ae62f2b7511a47ac58e2d8
Romain Beauxis <toots@rastageeks.org>
2014-04-17 18:24:53 -0500

    Switch to bootstrap, add start/stop button, remove marker when tracking is off.

--- www/js/tracker.js
+++ www/js/tracker.js
@@ -12,7 +12,7 @@
       var center;
       center = new google.maps.LatLng(29.9511782, -90.0689848);
       this.marker = null;
-      this.map = new google.maps.Map(document.getElementById("map_canvas"), {
+      this.map = new google.maps.Map(document.getElementById("map-canvas"), {
         disableDefaultUI: true,
         draggable: false,
         zoom: 15,
@@ -36,12 +36,20 @@
       return this.map.setCenter(position);
     };
 
+    Map.prototype.clearPosition = function() {
+      if (this.marker != null) {
+        this.marker.setMap(null);
+        return this.marker = null;
+      }
+    };
+
     return Map;
 
   })();
 
   Ui = (function() {
     function Ui() {
+      this.onClear = __bind(this.onClear, this);
       this.onPosition = __bind(this.onPosition, this);
     }
 
@@ -52,12 +60,17 @@
         port: serverPort
       });
       this.socket.on("position", this.onPosition);
+      this.socket.on("clear", this.onClear);
       return $("#position").text("Waiting for position..");
     };
 
     Ui.prototype.onPosition = function(position) {
       $("#position").html("Latitude: " + position.latitude + "<br/>\nLongitude: " + position.longitude);
       return this.map.setPosition(position.latitude, position.longitude);
+    };
+
+    Ui.prototype.onClear = function() {
+      return this.map.clearPosition();
     };
 
     return Ui;
@@ -68,11 +81,26 @@
     function BackgroundTracking() {
       this.onFailure = __bind(this.onFailure, this);
       this.onPosition = __bind(this.onPosition, this);
-      this.serverUrl = "" + serverHost + "/report.json";
+      var _ref;
+      this.reportUrl = "" + serverHost + "/report";
+      this.clearUrl = "" + serverHost + "/clear";
       window.navigator.geolocation.getCurrentPosition(function() {});
-      this.bgGeo = window.plugins.backgroundGeoLocation;
+      this.bgGeo = ((_ref = window.plugins) != null ? _ref.backgroundGeoLocation : void 0) || {
+        configure: function() {
+          return console.log("geo configure");
+        },
+        start: function() {
+          return console.log("geo start");
+        },
+        stop: function() {
+          return console.log("geo stop");
+        },
+        finish: function() {
+          return console.log("geo finish");
+        }
+      };
       this.bgGeo.configure(this.onPosition, this.onFailure, {
-        url: this.serverUrl,
+        url: this.reportUrl,
         desiredAccuracy: 0,
         stationaryRadius: 20,
         distanceFilter: 30
@@ -80,11 +108,22 @@
     }
 
     BackgroundTracking.prototype.start = function() {
-      return this.bgGeo.start();
+      var _this = this;
+      $("#start").removeAttr("disabled");
+      return $("#start").click(function() {
+        if ($("#start").hasClass("btn-success")) {
+          _this.bgGeo.start();
+          return $("#start").removeClass("btn-success").addClass("btn-danger").text("Stop Tracking");
+        } else {
+          _this.bgGeo.stop();
+          $("#start").removeClass("btn-danger").addClass("btn-success").text("Start Tracking");
+          return $.post(_this.clearUrl);
+        }
+      });
     };
 
     BackgroundTracking.prototype.onPosition = function(position) {
-      $.post(this.serverUrl, {
+      $.post(this.reportUrl, {
         location: position
       }, null, "json");
       return this.bgGeo.finish();
--- www/src/tracker.coffee
+++ www/src/tracker.coffee
@@ -8,7 +8,7 @@ class Map
     center = new google.maps.LatLng 29.9511782,-90.0689848 # Jackson square!
     @marker = null
 
-    @map = new google.maps.Map document.getElementById("map_canvas"),
+    @map = new google.maps.Map document.getElementById("map-canvas"),
       disableDefaultUI: true
       draggable:        false
       zoom:             15
@@ -29,6 +29,11 @@ class Map
 
     @map.setCenter position
 
+  clearPosition: ->
+    if @marker?
+      @marker.setMap null
+      @marker = null
+
 class Ui
   start: ->
     @map = new Map
@@ -37,6 +42,7 @@ class Ui
       port:       serverPort
 
     @socket.on "position", @onPosition
+    @socket.on "clear", @onClear
 
     $("#position").text "Waiting for position.."
 
@@ -48,25 +54,46 @@ class Ui
 
     @map.setPosition position.latitude, position.longitude
 
+  onClear: =>
+    @map.clearPosition()
+
 class BackgroundTracking
   constructor: ->
-    @serverUrl = "#{serverHost}/report.json"
+    @reportUrl = "#{serverHost}/report"
+    @clearUrl  = "#{serverHost}/clear"
 
     window.navigator.geolocation.getCurrentPosition -> # Dummy to prompt for right
 
-    @bgGeo = window.plugins.backgroundGeoLocation
+    @bgGeo = window.plugins?.backgroundGeoLocation ||
+      configure: ->
+        console.log "geo configure"
+      start: ->
+        console.log "geo start"
+      stop: ->
+        console.log "geo stop"
+      finish: ->
+        console.log "geo finish"
 
     @bgGeo.configure @onPosition, @onFailure,
-      url:              @serverUrl
+      url:              @reportUrl
       desiredAccuracy:  0
       stationaryRadius: 20
       distanceFilter:   30
 
   start: ->
-    @bgGeo.start()
+    $("#start").removeAttr "disabled"
+
+    $("#start").click =>
+      if $("#start").hasClass "btn-success"
+        @bgGeo.start()
+        $("#start").removeClass("btn-success").addClass("btn-danger").text "Stop Tracking"
+      else
+        @bgGeo.stop()
+        $("#start").removeClass("btn-danger").addClass("btn-success").text "Start Tracking"
+        $.post @clearUrl
 
   onPosition: (position) =>
-    $.post @serverUrl, {location: position}, null, "json"
+    $.post @reportUrl, {location: position}, null, "json"
     @bgGeo.finish()
 
   onFailure: (error) =>
